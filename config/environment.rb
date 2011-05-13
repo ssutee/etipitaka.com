a1a25b27b120476432e2561028fc81c91937b552
Sutee Sudprasert <etipitaka@gmail.com>
2011-05-13 09:44:05 +0700

    Added userbox nav

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Etipitaka::Application.initialize!
 
-config.gem "Chrononaut-no_fuzz", :source => "http://gems.github.com", :lib => "no_fuzz"
+# config.gem "Chrononaut-no_fuzz", :source => "http://gems.github.com", :lib => "no_fuzz"
