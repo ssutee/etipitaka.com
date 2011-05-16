8a5260e8b541641d849860c265581d4bc5c9b9b9
Sutee Sudprasert <etipitaka@gmail.com>
2011-05-16 08:04:46 +0700

    Changed server name in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,31 +2,31 @@
 $:.unshift(File.expand_path("~/.rvm/lib"))
 require 'rvm/capistrano'
 set :rvm_ruby_string, '1.9.2-p180'
-set :rvm_type, :user
+set :rvm_type, :system
 
 # bundler bootstrap
 require 'bundler/capistrano'
 
 #main detail
 
-set :application, "etipitaka.com"
-role :web, "etipitaka.com"                          # Your HTTP server, Apache/etc
-role :app, "etipitaka.com"                          # This may be the same as your `Web` server
-role :db,  "etipitaka.com", :primary => true # This is where Rails migrations will run
+set :application, "50.56.125.42"
+role :web, "50.56.125.42"                          # Your HTTP server, Apache/etc
+role :app, "50.56.125.42"                          # This may be the same as your `Web` server
+role :db,  "50.56.125.42", :primary => true # This is where Rails migrations will run
 
 # server details
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-set :deploy_to, "/var/www/etipitaka.com"
+set :deploy_to, "/var/www/www.etipitaka.com"
 set :deploy_via, :remote_cache
 set :user, "passenger"
 set :use_sudo, false
 
 # repo details
 set :scm, :git
-set :scm_username, "ssutee"
-set :repository, "git@github.com:etipitaka.com.git"
+set :scm_username, "git"
+set :repository, "git@github.com:ssutee/etipitaka.com.git"
 set :branch, "master"
 set :git_enable_submodules, 1
 
