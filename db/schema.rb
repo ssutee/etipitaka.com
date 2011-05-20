cec7682009cefcaa1ba0ea331194e83756fbb0b7
Sutee Sudprasert <etipitaka@gmail.com>
2011-05-20 21:04:42 +0700

    Deleted new model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110520135313) do
+ActiveRecord::Schema.define(:version => 20110520134739) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -78,13 +78,6 @@ ActiveRecord::Schema.define(:version => 20110520135313) do
     t.string   "title"
     t.string   "url"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "news", :force => true do |t|
-    t.string   "title"
-    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
